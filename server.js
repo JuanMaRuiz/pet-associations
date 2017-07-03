7488a324a90bd61e5c23e4c890bb284c9aa15e56
JuanMa Ruiz <juanmanuel.ruiz@edreamsodigeo.com>
2017-07-03 17:16:03 +0200

    Added basic configuration for eslint

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.get('/', function(req, res) {
 	res.send('Bazinga!!!!');
-})
+});
 
 app.listen(config.server, function() {
 	console.log(`App running in http://${config.server.host}:${config.server.port}`);
